9a8b6e6a021c5996b4c55e266f1af752bdfc207a
Calvin McDowell <calvinmac40@hotmail.com>
2019-09-16 21:47:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,6 +33,11 @@ class Song
     self.find_by_name(song_name) ||
     self.create_by_name(song_name)
   end
+  
+  def self.alphabetical(song_name)
+    self.sort {|song| song.name}
+  end
+  
 
 
 
